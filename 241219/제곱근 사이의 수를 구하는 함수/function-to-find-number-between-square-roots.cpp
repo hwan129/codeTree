43ea39680affe16d1f6ee124ab7919777d85b02d
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2024-12-19 23:13:02 +0900

    Title: 제곱근 사이의 수를 구하는 함수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241219/제곱근 사이의 수를 구하는 함수/function-to-find-number-between-square-roots.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a, b;
+
+    cin >> a >> b;
+
+    double a_sqrt = sqrt(a), b_sqrt = sqrt(b);
+    int num;
+
+    num = floor(a_sqrt) - floor(b_sqrt);
+
+    cout << num;
+
+
+
+
+
+
+    return 0;
+}
