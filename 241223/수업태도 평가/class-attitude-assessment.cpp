48de194480ec1e3e63bcba79d8304573b01d6de7
Seonghwan Sim <130568725+hwan129@users.noreply.github.com>
2024-12-23 20:51:19 +0900

    Title: 수업태도 평가, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241223/수업태도 평가/class-attitude-assessment.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string stu[] = {"Bessie", "Elsie", "Daisy", "Gertie", "Annabelle", "Maggie", "Henrietta"};
+    int score[] = {0,0,0,0,0,0,0};
+
+    int num;
+    cin >> num;
+
+    for(int i = 0; i < num; i++){
+        string student = "";
+        int sco;
+        cin >> student >> sco;
+        for(int j = 0; j < 7; j++){
+            if(student == stu[j]){
+                score[j] += sco;
+            }
+        }
+    }
+
+    int min = 0, scnd = 0, count = 0;
+    int mini[] = {100, 99};
+
+    for(int i = 0; i < 7; i++){
+        if(score[i] < mini[0]){
+            min = i;
+            mini[0] = score[i];
+        } else if(score[i] < mini[1] && score[i] > mini[0]){
+            scnd = i;
+            mini[1] = score[i];
+        } 
+        // cout << score[i] << " ";
+    }
+
+    // cout << scnd << endl;
+
+    for(int i = 0; i < 7; i++){
+        if(score[scnd] == score[i] && scnd != i){
+            cout << "Tie";
+            return 0;
+        }
+    }
+
+    cout << stu[scnd];
+
+    return 0;
+}
